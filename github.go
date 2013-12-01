a1017912dac0ffc7cbff8bd8e0f1113ccb797ef3
Michael Crosby <crosby.michael@gmail.com>
2013-12-01 10:01:14 -0800

    Move filtering to cli main
    Allow filters to be combined and composed

--- github.go
+++ github.go
@@ -2,10 +2,8 @@ package pulls
 
 import (
 	"fmt"
-	"github.com/codegangsta/cli"
 	gh "github.com/crosbymichael/octokat"
 	"strconv"
-	"strings"
 )
 
 // Top level type that manages a repository
@@ -69,43 +67,6 @@ func (m *Maintainer) GetPullRequest(number string, comments bool) (*gh.PullReque
 // Return all comments for an issue or pull request
 func (m *Maintainer) GetComments(number string) ([]gh.Comment, error) {
 	return m.client.Comments(m.repo, number, nil)
-}
-
-// Filter pull requests
-func (m *Maintainer) FilterPullRequests(prs []*gh.PullRequest, c *cli.Context) ([]*gh.PullRequest, error) {
-	out := []*gh.PullRequest{}
-	noMerge := c.Bool("no-merge")
-	fromUser := c.String("user")
-	lgtm := c.Bool("lgtm")
-
-	for _, pr := range prs {
-		fmt.Printf(".")
-		pr.ReviewComments = 0
-		if fromUser == "" || fromUser == pr.User.Login {
-			if noMerge {
-				pr, _, err := m.GetPullRequest(strconv.Itoa(pr.Number), false)
-				if err != nil {
-					return nil, err
-				}
-				if pr.Mergeable {
-					continue
-				}
-			}
-			if lgtm {
-				comments, err := m.GetComments(strconv.Itoa(pr.Number))
-				if err != nil {
-					return nil, err
-				}
-				for _, comment := range comments {
-					if strings.Contains(comment.Body, "LGTM") {
-						pr.ReviewComments += 1
-					}
-				}
-			}
-			out = append(out, pr)
-		}
-	}
-	return out, nil
 }
 
 // Add a comment to an existing pull request
